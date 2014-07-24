35bf869c1fe6e4c6147f0b40a9f75cc65c4ceca2
Alex Prudencio <alex.prudencio@gmail.com>
2014-07-23 21:07:38 -0700

    stats counter module

--- QuizWiz/Global.asax.cs
+++ QuizWiz/Global.asax.cs
@@ -1,21 +1,88 @@
-﻿using QuizWiz.Models;
-using System.Data.Entity;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace QuizWiz
+﻿namespace QuizWiz
 {
-    // Note: For instructions on enabling IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=301868
+    using System;
+    using System.Configuration;
+    using System.Data.SQLite;
+    using System.Threading;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    /// <summary>
+    /// 
+    /// </summary>
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly object lockObj = new object();
+        private Timer timer;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MvcApplication()
+            : base()
+        { //WARNING: might be called multiple times per App Domain. Application_Start called only once.
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        private void OnTimerElapsed(object sender)
+        {
+            bool updated = false;
+            long bytesSent = 0;
+
+            try
+            {
+                lock (lockObj)
+                {
+                    bytesSent = Interlocked.Read(ref Global.bytesSent);
+                    Interlocked.Add(ref Global.bytesSent, -bytesSent);
+                }
+
+                using (var conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    conn.Open();
+
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "UPDATE Stats SET Value = Value + " + bytesSent + " WHERE Name = 'BytesSent'";
+                        cmd.ExecuteNonQuery();
+                        updated = true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                if (updated)
+                {
+                    Interlocked.Add(ref Global.bytesSent, bytesSent);
+                }
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            this.timer = new Timer(OnTimerElapsed);
+            this.timer.Change(TimeSpan.Zero, TimeSpan.FromMinutes(1));
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Global
+    {
+        public static long bytesSent;
     }
 }
---
+++ QuizWiz/Web/CounterModule.cs
@@ -0,0 +1,175 @@
+﻿namespace QuizWiz.Web
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Web;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CounterModule : IHttpModule
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += context_BeginRequest;
+            context.EndRequest += context_EndRequest;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void context_EndRequest(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = sender as HttpApplication;
+            application.Context.Response.Filter = new StreamCounter(application.Context.Response.Filter);
+
+            if (Global.bytesSent > 2000000)
+            {
+                application.Context.Response.Close();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StreamCounter : Stream
+    {
+        private Stream original;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="original"></param>
+        public StreamCounter(Stream original)
+            : base()
+        {
+            this.original = original;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override long Length
+        {
+            get { return 0; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override long Position
+        {
+            get { return this.original.Position; }
+            set { this.original.Position = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public override long Seek(long offset, System.IO.SeekOrigin direction)
+        {
+            return this.original.Seek(offset, direction);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="length"></param>
+        public override void SetLength(long length)
+        {
+            this.original.SetLength(length);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Close()
+        {
+            this.original.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Flush()
+        {
+            this.original.Flush();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return this.original.Read(buffer, offset, count);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            Interlocked.Add(ref Global.bytesSent, count);
+            this.original.Write(buffer, offset, count);
+        }
+    }
+}
